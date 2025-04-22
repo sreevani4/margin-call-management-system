fd69c4e3e52a0e348801360823767ba8e0d5ce07
sreevani <vattemsreevani@gmail.com>
2025-04-22 01:58:18 +0000

    Initial commit with margin call management system

---
+++ margin-call-management-system/src/main/java/com/margin/MarginCallManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.margin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarginCallManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarginCallManagementSystemApplication.class, args);
+	}
+
+}
---
+++ margin-call-management-system/src/test/java/com/margin/MarginCallManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarginCallManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
